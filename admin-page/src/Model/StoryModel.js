3efa6d44aaf9f2d8cb898572d18f44b9ed19e77b
Jamal Sohail <s-sohail@hotmail.de>
2020-04-20 09:36:19 +0200

    first version admin panel

---
+++ admin-page/src/Model/StoryModel.js
@@ -0,0 +1,8 @@
+export class SolutionModel {
+    constructor(id, title, content, pages) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.pages = pages;
+    }
+}
