28736c754d9f65400d16c7c82ba2f7ebbfe492fb
Onur Kilimci <onur.kilimci@tum.de>
2020-04-26 15:05:57 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Puzzle from "./Puzzle";
+
+function App() {
+  return (
+    <div className="App">
+      <Puzzle/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Puzzle.js
@@ -0,0 +1,87 @@
+import React, {Component} from "react";
+import originalImage from './images/Hargid.jpg'
+import './Puzzle.css'
+
+class  Puzzle extends Component{
+    state = {
+        pieces: [],
+        shuffled: [],
+        solved: []
+    };
+    componentDidMount() {
+        const pieces = [...Array(9)]
+            .map((_, i) => (
+                {
+                    img: `hargid_${('0' + (i + 1)).substr(-2)}.jpg`,
+                    order: i,
+                    board: 'shuffled'
+                }
+            ));
+
+        this.setState({
+            pieces,
+            shuffled: this.shufflePieces(pieces),
+            solved: [...Array(9)]
+        });
+    }
+    shufflePieces(pieces) {
+        const shuffled = [...pieces];
+
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+
+        return shuffled;
+    }
+    render() {
+        return (
+            <div className="jigsaw">
+                <ul className="jigsaw__shuffled-board">
+                    {
+                        this.state.shuffled.map((piece, i) =>
+                            this.renderPieceContainer(piece, i, 'shuffled'))
+                    }
+                </ul>
+                <ol className="jigsaw__solved-board" style={{ backgroundImage: `url(${originalImage})` }}>
+                    {
+                        this.state.solved.map((piece, i) =>
+                            this.renderPieceContainer(piece, i, 'solved'))
+                    }
+                </ol>
+            </div>
+        );
+    }
+    renderPieceContainer(piece, index, boardName) {
+        return (
+            <li key={index}
+                onDragOver={event => event.preventDefault()}
+                onDrop={event => this.handleDrop(event,index,boardName)}>
+                {piece && <img
+                    draggable
+                    onDragStart={(event => this.handleDragStart(event,piece.order))}
+                    src={require(`./images/${piece.img}`)}/>}
+            </li>
+        );
+    }
+    handleDragStart(e, order) {
+        e.dataTransfer.setData('text/plain', order);
+    }
+
+    handleDrop(e, index, targetName) {
+        let target = this.state[targetName];
+        if (target[index]) return;
+
+        const pieceOrder = e.dataTransfer.getData('text');
+        const pieceData = this.state.pieces.find(p => p.order === +pieceOrder);
+        const origin = this.state[pieceData.board];
+
+        if (targetName === pieceData.board) target = origin;
+        origin[origin.indexOf(pieceData)] = undefined;
+        target[index] = pieceData;
+        pieceData.board = targetName;
+        this.setState({ [pieceData.board]: origin, [targetName]: target })
+    }
+}
+
+export default Puzzle
